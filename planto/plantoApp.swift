0785d8d03cdb3b093a40548251a946388b9bf3db
lyan <lyankh02@gmail.com>
2025-10-21 14:32:43 +0300

    Initial Commit

---
+++ planto/plantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantoApp.swift
+//  planto
+//
+//  Created by Lyan on 29/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plantoTests/plantoTests.swift
@@ -0,0 +1,17 @@
+//
+//  plantoTests.swift
+//  plantoTests
+//
+//  Created by Lyan on 29/04/1447 AH.
+//
+
+import Testing
+@testable import planto
+
+struct plantoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
